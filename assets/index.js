d05df3d9f319388f2f052b534fca18c96cf893ee
景昇 <asfo@qq.com>
2025-06-28 00:15:36 +0800

    refactor: 重构页面切换

--- assets/index.js
+++ assets/index.js
@@ -11,12 +11,15 @@ const $ = dom => {
 	return document.querySelector(dom)
 }
 
+const nav = $('nav')
+const main = $('main')
 const link = $('#link')
 const line = $('#line')
 const site = $('#site')
 const music = $('audio')
 const darkMode = $('#darkMode')
 const dom = document.documentElement
+const sections = document.querySelectorAll('section')
 
 let theme = JSON.parse(localStorage.getItem('dark')) || false
 
@@ -93,39 +96,37 @@ fetch('/data.json')
 	})
 
 // 点击页面切换
-document.querySelector('nav').addEventListener('click', e => {
+nav.addEventListener('click', ({ target }) => {
 	// 事件委托,判断点击的元素是否为A标签
-	if (e.target.tagName === 'A') {
-		// 阻止默认事件
-		e.preventDefault()
+	if (target.tagName === 'SPAN') {
+		// 如果已是active，避免无效操作
+		if (target.classList.contains('active')) return
 		// 先移除active类名,再给当前点击的元素添加active类名
-		document.querySelector('.active').classList.remove('active')
-		e.target.classList.add('active')
-		// 获取name为当前点击元素id值的元素的offsetLeft值
-		const num = document.querySelector(`[title='${e.target.id}']`).offsetLeft
-		document.querySelector('main').scrollTo({ left: num, behavior: 'smooth' })
+		$('.active').classList.remove('active')
+		target.classList.add('active')
+		// 滚动到当前点击元素id值对应元素的offsetLeft处
+		const num = $(`[title='${target.id}']`).offsetLeft
+		main.scrollLeft = num
 	}
 })
 
 // 滑动页面切换Tab
-let timer = null
-// 监听滚动事件，并设置防抖
-document.querySelector('main').addEventListener('scroll', e => {
-	// 先清除定时器
-	if (timer) clearTimeout(timer)
-	// 设置定时器
-	timer = setTimeout(() => {
-		// 获取所有的section元素并遍历
-		document.querySelectorAll('section').forEach(item => {
-			// 判断当前元素的offsetLeft值与滚动条的scrollLeft值的差的绝对值是否小于误差阈值
-			if (Math.abs(e.target.scrollLeft - item.offsetLeft) < 50) {
-				// 先移除active类名，再给当前元素对应的A标签添加active类名
-				document.querySelector('.active').classList.remove('active')
-				document.querySelector(`#${item.title}`).classList.add('active')
-				return
-			}
-		})
-	}, 200) // 延迟200毫秒执行
+main.addEventListener('scroll', e => {
+	// 获取所有的section元素并遍历
+	sections.forEach(item => {
+		// 判断当前元素的offsetLeft值与滚动条的scrollLeft差值的绝对值是否小于元素一半宽度
+		if (
+			Math.abs(e.target.scrollLeft - item.offsetLeft) <
+			item.getBoundingClientRect().width / 2
+		) {
+			// 如果已是active，避免无效操作
+			if ($(`#${item.title}`).classList.contains('active')) return
+			// 先移除active类名，再给当前元素对应的标签添加active类名
+			$('.active').classList.remove('active')
+			$(`#${item.title}`).classList.add('active')
+			return
+		}
+	})
 })
 
 // 音乐功能
